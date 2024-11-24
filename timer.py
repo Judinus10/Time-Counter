94ebf411b08a8e0bcf261f27e31bf27bbf79fe0d
judinus10 <judes.20222403@iit.ac.lk>
2024-11-24 10:58:36 +0530

    created a basic Tkinter GUI application for a "Timer."

--- timer.py
+++ timer.py
@@ -1,5 +1,12 @@
 from tkinter import * 
 from playsound import playsound
 import time
-iiid
+
+root=Tk()
+root.title("Timer")
+root.geometry("400x600")
+root.config(bg="#000")
+root.resizable(False,False)
+
+root.mainloop()
 
