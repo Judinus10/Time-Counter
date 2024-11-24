41793d05692e4e9c17db9481015eb1c3d5c876a2
judinus10 <judes.20222403@iit.ac.lk>
2024-11-24 11:01:10 +0530

    added heading

--- timer.py
+++ timer.py
@@ -8,5 +8,7 @@ root.geometry("400x600")
 root.config(bg="#000")
 root.resizable(False,False)
 
+heading=Label(root,text="Timer",font="arial 30 bold",bg="#000",fg="#ea3548")
+heading.pack(pady=10)
 root.mainloop()
 
